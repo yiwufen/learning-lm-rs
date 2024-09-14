3a214550a92d72c40d610bbb9c6f0f2e057ccc6a
LiHang <yiyiyiwufeng@gmail.com>
2024-09-14 20:42:46 +0800

    story version

--- src/main.rs
+++ src/main.rs
@@ -13,17 +13,23 @@ fn main() {
     let model_dir = PathBuf::from(project_dir).join("models").join("story");
     let llama = model::Llama::<f32>::from_safetensors(&model_dir);
     let tokenizer = Tokenizer::from_file(model_dir.join("tokenizer.json")).unwrap();
-    let input = "Once upon a time";
-    let binding = tokenizer.encode(input, true).unwrap();
-    let input_ids = binding.get_ids();
-    // print!("\n{}", input);
-    let output_ids = llama.generate(
-        input_ids,
-        500,
-        0.9,
-        4,
-        1.,
-    );
-    println!("{}", tokenizer.decode(&output_ids, true).unwrap());
+    loop {
+        let mut input = String::new();
+        std::io::stdin().read_line(&mut input).unwrap();
+        let input = input.trim().to_string();
+        if input == "exit" {
+            break;
+        }
+        let binding = tokenizer.encode(input.clone(), true).unwrap();
+        let input_ids = binding.get_ids();
+        // print!("\n{}", input);
+        let output_ids = llama.generate(
+            input_ids,
+            500,
+            0.9,
+            4,
+            1.,
+        );
+        println!("{} {}", input, tokenizer.decode(&output_ids, true).unwrap());
+    }
 }
-
